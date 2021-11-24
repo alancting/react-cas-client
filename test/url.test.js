2583e8177e32d7db2924e45e9c609d074ef3da11
Alan Si <32054659+alancting@users.noreply.github.com>
2021-11-25 07:35:02 +0800

    Added better proxy support (#11) (#12)

--- test/url.test.js
+++ test/url.test.js
@@ -165,71 +165,145 @@ describe('getValidateUrl()', () => {
     [constant.CAS_VERSION_3_0, '/p3/serviceValidate', '&format=json'],
   ])('cas version: %p', (casVersion, casServicePath, casExtraParamStr) => {
     describe.each([['http'], ['https']])('cas protocol: %p', (casProtocol) => {
-      describe.each([
-        [true, 'https://mock.testing.com/mock_proxy_path/cas-path'],
-        [false, casProtocol + '://fake.cas.com/cas-path'],
-      ])(
-        'validate proxy path set: %p',
-        (proxyPathSet, expectedValidationUrlLeading) => {
-          describe.each([
-            ['index', () => {}, 'https://mock.testing.com/'],
-            [
-              'index with extra params',
-              () => {
-                window.history.pushState(null, null, '/?other_params=1234');
-              },
-              'https://mock.testing.com/?other_params=1234',
-            ],
-            [
-              'dashboard',
-              () => {
-                window.history.pushState(null, null, '/dashboard');
-              },
-              'https://mock.testing.com/dashboard',
-            ],
-            [
-              'dashboard with extra prarams',
-              () => {
-                window.history.pushState(
-                  null,
-                  null,
-                  '/dashboard?other_params=1234'
-                );
-              },
-              'https://mock.testing.com/dashboard?other_params=1234',
-            ],
-          ])(
-            'current page is %p',
-            (pageName, changePageAction, expectedReturnUrl) => {
-              beforeEach(() => {
-                changePageAction();
-              });
+      describe.each([[], ['http://mock.proxy.com/callback']])(
+        'cas proxy callback url: %p',
+        (proxyCallbackUrl) => {
+          describe.each([[], [true], [false]])(
+            'validation proxy: %p',
+            (validationProxy) => {
+              describe.each([[''], ['http'], ['https']])(
+                'validation proxy protocal: %p',
+                (validationProxyProtocol) => {
+                  describe.each([[''], ['validation.proxy.com']])(
+                    'validation proxy endpoint: %p',
+                    (validationProxyEndpoint) => {
+                      describe.each([[''], ['/validation_proxy_path']])(
+                        'validation proxy path: %p',
+                        (validationProxyPath) => {
+                          describe.each([
+                            ['index', () => {}, 'https://mock.testing.com/'],
+                            [
+                              'index with extra params',
+                              () => {
+                                window.history.pushState(
+                                  null,
+                                  null,
+                                  '/?other_params=1234'
+                                );
+                              },
+                              'https://mock.testing.com/?other_params=1234',
+                            ],
+                            [
+                              'dashboard',
+                              () => {
+                                window.history.pushState(
+                                  null,
+                                  null,
+                                  '/dashboard'
+                                );
+                              },
+                              'https://mock.testing.com/dashboard',
+                            ],
+                            [
+                              'dashboard with extra prarams',
+                              () => {
+                                window.history.pushState(
+                                  null,
+                                  null,
+                                  '/dashboard?other_params=1234'
+                                );
+                              },
+                              'https://mock.testing.com/dashboard?other_params=1234',
+                            ],
+                          ])(
+                            'current page is %p',
+                            (pageName, changePageAction, expectedReturnUrl) => {
+                              beforeEach(() => {
+                                changePageAction();
+                              });
 
-              afterEach(() => {
-                window.history.replaceState(null, null, '/');
-              });
+                              afterEach(() => {
+                                window.history.replaceState(null, null, '/');
+                              });
 
-              test('return url', () => {
-                let casParams = {
-                  path: '/cas-path',
-                  version: casVersion,
-                  protocol: casProtocol,
-                };
-                if (proxyPathSet) {
-                  casParams.validation_proxy_path = '/mock_proxy_path';
+                              test('return url', () => {
+                                let casParams = {
+                                  path: '/cas-path',
+                                  version: casVersion,
+                                  protocol: casProtocol,
+                                };
+                                if (proxyCallbackUrl) {
+                                  casParams.proxy_callback_url =
+                                    proxyCallbackUrl;
+                                }
+                                if (validationProxy) {
+                                  casParams.validation_proxy = validationProxy;
+                                }
+                                if (validationProxyProtocol) {
+                                  casParams.validation_proxy_protocol =
+                                    validationProxyProtocol;
+                                }
+                                if (validationProxyEndpoint) {
+                                  casParams.validation_proxy_endpoint =
+                                    validationProxyEndpoint;
+                                }
+                                if (validationProxyPath) {
+                                  casParams.validation_proxy_path =
+                                    validationProxyPath;
+                                }
+                                let expectedValidationUrlLeading = '';
+                                if (!validationProxy) {
+                                  expectedValidationUrlLeading =
+                                    casProtocol + '://fake.cas.com/cas-path';
+                                } else {
+                                  let protocal = 'https';
+                                  let endpoint = 'mock.testing.com';
+                                  let path = '';
+                                  if (validationProxyProtocol) {
+                                    protocal = validationProxyProtocol;
+                                  }
+                                  if (validationProxyEndpoint) {
+                                    endpoint = validationProxyEndpoint;
+                                  }
+                                  if (validationProxyPath) {
+                                    path = validationProxyPath;
+                                  }
+                                  expectedValidationUrlLeading =
+                                    protocal + '://' + endpoint + path;
+                                }
+
+                                let expectedValidationUrl =
+                                  expectedValidationUrlLeading +
+                                  casServicePath +
+                                  '?service=' +
+                                  encodeURIComponent(expectedReturnUrl) +
+                                  '&ticket=Mock-Ticket-123456786543212345678' +
+                                  casExtraParamStr;
+                                if (proxyCallbackUrl) {
+                                  expectedValidationUrl +=
+                                    '&pgtUrl=' +
+                                    encodeURIComponent(proxyCallbackUrl);
+                                }
+
+                                const cas = new CasClient(
+                                  'fake.cas.com',
+                                  casParams
+                                );
+                                expect(
+                                  url.getValidateUrl(
+                                    cas,
+                                    'Mock-Ticket-123456786543212345678'
+                                  )
+                                ).toBe(expectedValidationUrl);
+                              });
+                            }
+                          );
+                        }
+                      );
+                    }
+                  );
                 }
-                let expectedValidationUrl =
-                  expectedValidationUrlLeading +
-                  casServicePath +
-                  '?service=' +
-                  encodeURIComponent(expectedReturnUrl) +
-                  '&ticket=Mock-Ticket-123456786543212345678' +
-                  casExtraParamStr;
-                let cas = new CasClient('fake.cas.com', casParams);
-                expect(
-                  url.getValidateUrl(cas, 'Mock-Ticket-123456786543212345678')
-                ).toBe(expectedValidationUrl);
-              });
+              );
             }
           );
         }
